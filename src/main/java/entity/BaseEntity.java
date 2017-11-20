1d80b211572d9b191cc2832362cfbe93ca1c476a
Stephan Girod <lick58@gmx.net>
2017-11-20 09:05:10 +0100

    Refactored id in BaseEntity

--- src/main/java/entity/BaseEntity.java
+++ src/main/java/entity/BaseEntity.java
@@ -8,8 +8,8 @@ public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, columnDefinition = "BIGINT UNSIGNED")
-    private Long nr;
+    @Column(nullable = false, columnDefinition = "BIGINT UNSIGNED")
+    private Long id;
 
     @Version
     private int version;
@@ -24,8 +24,8 @@ public abstract class BaseEntity {
 
     protected String updatedBy;
 
-    public Long getNr() {
-        return nr;
+    public Long getId() {
+        return id;
     }
 
     public int getVersion() {
@@ -43,23 +43,28 @@ public abstract class BaseEntity {
     }
 
     @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (this.getNr() != null ? this.getNr().hashCode() : 0);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-        return hash;
+        BaseEntity that = (BaseEntity) o;
+
+        if (version != that.version) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (createdAt != null ? !createdAt.equals(that.createdAt) : that.createdAt != null) return false;
+        if (createdBy != null ? !createdBy.equals(that.createdBy) : that.createdBy != null) return false;
+        if (updatedAt != null ? !updatedAt.equals(that.updatedAt) : that.updatedAt != null) return false;
+        return updatedBy != null ? updatedBy.equals(that.updatedBy) : that.updatedBy == null;
     }
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object)
-            return true;
-        if (object == null)
-            return false;
-        if (getClass() != object.getClass())
-            return false;
-
-        BaseEntity other = (BaseEntity) object;
-        return this.getNr() == other.getNr() || (this.getNr() != null && this.nr.equals(other.nr));
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + version;
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        result = 31 * result + (createdBy != null ? createdBy.hashCode() : 0);
+        result = 31 * result + (updatedAt != null ? updatedAt.hashCode() : 0);
+        result = 31 * result + (updatedBy != null ? updatedBy.hashCode() : 0);
+        return result;
     }
 }
