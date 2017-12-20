6699948780b9c69c7efbe59b076857a6c5cbe008
Adrian Krebs <krebs-adrian@bluewin.ch>
2017-12-20 18:21:10 +0100

    fix: minor improvements

--- src/main/java/ch/bfh/eadj/application/service/MailService.java
+++ src/main/java/ch/bfh/eadj/application/service/MailService.java
@@ -27,8 +27,7 @@ public class MailService {
     public Future<String> sendProccessStartedMail(Order order) {
         Customer c = order.getCustomer();
         return sendMail(order, "Hello " + c.getFirstName() + ". We just started processing your order. We'll notify you as soon as we shipped the order",
-                "Order processing started",
-                "adrian_krebs@outlook.com");
+                "Order processing started");
     }
 
 
@@ -36,20 +35,19 @@ public class MailService {
     public Future<String> sendOrderShippedMail(Order order) {
         Customer c = order.getCustomer();
         return sendMail(order, "Hello " + c.getFirstName() + "Hello " + c.getFirstName() + ". Your book order " + order.getNr() + " has been shipped",
-                "Order shipped",
-                c.getEmail());
+                "Order shipped");
     }
 
 
     // 573 5.1.1 Swisscom Antispam: Authentifizierte Verbindung nicht moeglich. Bitte benutzen Sie den Port 587 oder 465 (SSL/TLS) anstelle von Port 25. Weitere Informationen: www.swisscom.ch/p25. Connexion authentifiee pas possible. Veuillez utiliser le port 587 ou 465 (SSL/TLS) a la place du port 25. Ulterieurs informations: www.swisscom.ch/p25. Collegamento autenticato non e possibile. Si prega di utilizzare la porta 587 o 465 (SSL/TLS) invece di porta 25. Altra informazione: www.swisscom.ch/p25. Authenticated connection is not possible. Please use port 587 or 465 (SSL/TLS) instead of port 25. More information: www.swisscom.ch/p25.
     // 550 5.7.60 SMTP; Client does not have permissions to send as this sender
-    private Future<String> sendMail(Order order, String text, String subject, String mailAddress) {
+    private Future<String> sendMail(Order order, String text, String subject) {
         String status;
         try {
             MimeMessage msg = new MimeMessage(mailSession);
             msg.setFrom("me@example.com");
             msg.setRecipients(Message.RecipientType.TO,
-                    "adrian_krebs@outlook.com");
+                    order.getCustomer().getEmail());
             msg.setSubject(subject);
             msg.setSentDate(new Date());
             msg.setText(text);
