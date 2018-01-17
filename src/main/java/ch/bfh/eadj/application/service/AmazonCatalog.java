e17da548be104a983f7fd487221310248399ee42
Adrian Krebs <krebs-adrian@bluewin.ch>
2018-01-17 18:42:07 +0100

    feat: generated sources

--- src/main/java/ch/bfh/eadj/application/service/AmazonCatalog.java
+++ src/main/java/ch/bfh/eadj/application/service/AmazonCatalog.java
@@ -3,31 +3,51 @@ package ch.bfh.eadj.application.service;
 import ch.bfh.eadj.application.exception.BookNotFoundException;
 import ch.bfh.eadj.persistence.dto.BookInfo;
 import ch.bfh.eadj.persistence.entity.Book;
-import org.contacts.soap.AWSECommerceService;
-import org.contacts.soap.AWSECommerceServicePortType;
+import org.contacts.soap.*;
 
 import javax.ejb.Stateless;
 import javax.xml.ws.WebServiceRef;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Stateless
 public class AmazonCatalog {
+    public static final String MEDIUM = "Medium";
+    public static final String  SEARCH_INDEX = "Books";
 
     @WebServiceRef(AWSECommerceService.class)
     private AWSECommerceServicePortType port;
 
 
     public Book findBook(String isbn) throws BookNotFoundException {
-        //port.itemLookup()
+        ItemLookup lookup = new ItemLookup();
+        ItemLookupRequest request = lookup.getShared();
+        request.setIdType(isbn);
+        request.setSearchIndex(SEARCH_INDEX);
+        request.getResponseGroup().add(MEDIUM);
+        //TODO productGroup
+        ItemLookupResponse itemLookupResponse = this.port.itemLookup(lookup);
+        //TODO extract response
+        System.out.println(itemLookupResponse);
+        return null;
 
 
     }
 
 
     public List<BookInfo> searchBooks(String keywords) {
-        //port.itemSearch();
+
+        ItemSearch search = new ItemSearch();
+        ItemSearchRequest shared = search.getShared();
+
+        shared.setSearchIndex(SEARCH_INDEX);
+        shared.setKeywords(keywords);
+
+        ItemSearchResponse itemSearchResponse = port.itemSearch(search);
+
+        //TODO extract response
+        System.out.println(itemSearchResponse);
+        return null;
+
     }
 
 
