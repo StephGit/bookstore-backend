0f5583e8e48de124de911bcff22fc2d9c050122f
Adrian Krebs <krebs-adrian@bluewin.ch>
2018-01-17 18:24:38 +0100

    feat: generated sources

--- src/main/java/ch/bfh/eadj/application/service/AmazonCatalog.java
+++ src/main/java/ch/bfh/eadj/application/service/AmazonCatalog.java
@@ -1,9 +1,34 @@
 package ch.bfh.eadj.application.service;
 
+import ch.bfh.eadj.application.exception.BookNotFoundException;
+import ch.bfh.eadj.persistence.dto.BookInfo;
+import ch.bfh.eadj.persistence.entity.Book;
+import org.contacts.soap.AWSECommerceService;
+import org.contacts.soap.AWSECommerceServicePortType;
+
 import javax.ejb.Stateless;
+import javax.xml.ws.WebServiceRef;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Stateless
 public class AmazonCatalog {
+
+    @WebServiceRef(AWSECommerceService.class)
+    private AWSECommerceServicePortType port;
+
+
+    public Book findBook(String isbn) throws BookNotFoundException {
+        //port.itemLookup()
+
+
+    }
+
+
+    public List<BookInfo> searchBooks(String keywords) {
+        //port.itemSearch();
+    }
 
 
 
