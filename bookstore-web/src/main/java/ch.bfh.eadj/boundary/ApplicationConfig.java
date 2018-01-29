c5e71119e54ba43d6bf39f555820d2ba2b0ea15d
Adrian Krebs <krebs-adrian@bluewin.ch>
2018-01-29 11:09:05 +0100

    refactor: removed unused code in appconfig

--- bookstore-web/src/main/java/ch.bfh.eadj/boundary/ApplicationConfig.java
+++ bookstore-web/src/main/java/ch.bfh.eadj/boundary/ApplicationConfig.java
@@ -2,17 +2,8 @@ package ch.bfh.eadj.boundary;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("api")
 public class ApplicationConfig extends Application {
 
-	@Override
-	public Set<Class<?>> getClasses() {
-		Set<Class<?>> classes = new HashSet<>();
-		classes.add(CatalogResource.class);
-		classes.add(CustomerResource.class);
-		return classes;
-	}
 }
